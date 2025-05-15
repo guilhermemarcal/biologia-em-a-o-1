69bbac0780bc0af4d35d85804d62ca356c87dafe
guilhermemarcal <guilherme.marcal.barbosa@escola.pr.gov.br>
2025-05-15 07:50:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é o reino plantae?',
+    'O reino plantae são todas as plantas'
+)
+
+criaCartao(
+    'Qual o nome científico do hipopótamo?',
+    'Hippopotamus amphibius'
+)
+
+criaCartao(
+    'O que são células eucariontes?',
+    'Células eucariontes são todas as células que tem núcleo'
+)
+
+criaCartao(
+    'O que é abiogênese?',
+    'É a vida que surge da não vida'
+)
